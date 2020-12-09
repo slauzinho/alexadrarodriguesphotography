2a9af818d1fc6e7aa9eafbc875621dfa388f6420
teerryn <slaughterer90@gmail.com>
2020-12-09 11:51:20 +0000

    Add review component

---
+++ src/components/Reviews.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Reviews = ({ reviews = [] }) => {
+  return (
+    <div className="flex px-12 mt-24 flex-wrap sm:justify-center items-center">
+      {reviews.map(review => (
+        <div className="px-4 py-4">
+          <h3>{review.name}</h3>
+          <p>{review.review}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Reviews
