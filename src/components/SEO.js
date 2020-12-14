c6c48a8eab8b23323fc9b19554ae0aa11b4e5711
teerryn <slaughterer90@gmail.com>
2020-12-14 10:58:20 +0000

    Add css to header

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -33,6 +33,17 @@ export default function SEO({ children, location, description, title }) {
         key="ogsitename"
       />
       <meta property="og:description" content={description} key="ogdesc" />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        charset="UTF-8"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+      />
       {children}
     </Helmet>
   )
